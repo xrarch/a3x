8551f15f9091d91dcfd529cff860e75d40297543
ash <ashnwill49@gmail.com>
2019-05-06 19:03:35 -0600

    change ABI and make runtime cooler

--- src/API.d
+++ src/API.d
@@ -1,134 +1,23 @@
 table API
-	pointerof APIPutc
-	pointerof APIGetc
-	pointerof APIGets
-	pointerof APIPuts
+	pointerof Putc
+	pointerof Getc
+	pointerof Gets
+	pointerof Puts
 	pointerof APIDevTree
-	pointerof APIMalloc
-	pointerof APICalloc
-	pointerof APIFree
+	pointerof Malloc
+	pointerof Calloc
+	pointerof Free
+
+	pointerof DevTreeWalk
+	pointerof DeviceParent
+	pointerof DeviceSelectNode
+	pointerof DeviceSelect
+	pointerof DGetProperty
+	pointerof DGetMethod
+	pointerof DCallMethod
+	pointerof DeviceExit
 endtable
 
-procedure APIMalloc
-	asm "
-
-	;r0 - size
-
-	pushv r5, r0
-	call Malloc
-	popv r5, r0
-
-	;r0 - ptr
-
-	"
-end
-
-procedure APICalloc
-	asm "
-
-	;r0 - size
-
-	pushv r5, r0
-	call Calloc
-	popv r5, r0
-
-	;r0 - ptr
-
-	"
-end
-
-procedure APIFree
-	asm "
-
-	;r0 - ptr
-
-	pushv r5, r0
-	call Free
-
-	"
-end
-
-procedure APIPutc
-	asm "
-
-	;r0 - char
-
-	pushv r5, r0
-	call Putc
-
-	"
-end
-
-procedure APIGetc
-	asm "
-
-	call Getc
-	popv r5, r0
-
-	;r0 - char
-
-	"
-end
-
-procedure APIGets
-	asm "
-
-	;r0 - s
-	;r1 - max
-
-	pushv r5, r0
-	pushv r5, r1
-
-	call Gets
-
-	"
-end
-
-procedure APIPuts
-	asm "
-
-	;r0 - string
-
-	pushv r5, r0
-
-	call Puts
-
-	"
-end
-
-procedure APIPutx
-	asm "
-
-	;r0 - x
-
-	pushv r5, r0
-
-	call Putx
-
-	"
-end
-
-procedure APIPutn
-	asm "
-
-	;r0 - n
-
-	pushv r5, r0
-
-	call Putn
-
-	"
-end
-
-procedure APIDevTree
-	DevTree@ asm "
-
-	li r1, DevCurrent
-
-	popv r5, r0
-
-	;r0 - devtree
-	;r1 - devcurrent
-
-	"
+procedure APIDevTree (* -- root dcp *)
+	DevCurrent@ DevTree@
 end
