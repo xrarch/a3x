ee6089638b3b823b9df1b8dacf60106969efabc3
hyenasky <ashnwill49@gmail.com>
2020-10-11 08:40:06 -0600

    add a multiboot picker

--- src/include/boot.h
+++ src/include/boot.h
@@ -3,6 +3,8 @@ struct BootRecord
 	16 OSLabel
 	4 BootBlockStart
 	4 BootBlockCount
+	4 Reserved
+	512 BadgeBitmap
 endstruct
 
 const BootMagic 0x45544E41
@@ -14,6 +16,10 @@ externptr BootErrors (* table *)
 
 externptr API (* table *)
 
+externptr BootableDevs (* table *)
+
+externptr NumBootableDevs (* var *)
+
 extern DevBootable { devnode -- bootable }
 
 extern DevBootableR { devnode -- brecord bootable }
@@ -22,4 +28,6 @@ extern AutoBoot { -- ok }
 
 extern BootNode { devnode args -- ok }
 
-extern BootUI { -- }
+extern BootUI { -- node }
+
+extern AddBootable { -- }
