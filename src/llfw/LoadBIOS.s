9e6c0c604917cbaae2d00ed235716c7238cd4566
hyenasky <ashnwill49@gmail.com>
2022-08-01 01:45:40 -0600

    make llfw support loading an XLOFF bios image

--- src/llfw/LoadBIOS.s
+++ src/llfw/LoadBIOS.s
@@ -9,39 +9,47 @@
 .extern memcpy
 .extern memset
 
-.struct LOFF
+;we assume here that text, data, and bss sections will be 0, 1, and 2
+;respectively.
+
+.struct XLOFF
 	4 Magic
 	4 SymbolTableOffset
 	4 SymbolCount
 	4 StringTableOffset
 	4 StringTableSize
-	4 Architecture
+	4 TargetArchitecture
 	4 EntrySymbol
-	4 Stripped
-	28 Reserved
-	4 TextHeader
-	4 DataHeader
-	4 BSSHeader
+	4 Flags
+	4 Timestamp
+	4 SectionTableOffset
+	4 SectionCount
+	4 ImportTableOffset
+	4 ImportCount
+	4 HeadLength
 .end-struct
 
 .struct Symbol
 	4 NameOffset
-	4 Section
-	4 Type
 	4 Value
-	4 ImportIndex
+	2 SectionIndexI
+	1 TypeB
+	1 FlagsB
 .end-struct
 
-.struct Header
-	4 FixupTableOffset
-	4 FixupCount
-	4 SectionOffset
-	4 SectionSize
-	4 LinkedAddress
+.struct SectionHeader
+	4 NameOffset
+	4 DataOffset
+	4 DataSize
+	4 VirtualAddress
+	4 RelocTableOffset
+	4 RelocCount
+	4 Flags
+	0 SIZEOF
 .end-struct
 
-.define LOFFMagic 0x4C4F4635
-.define LOFFArch  5
+.define XLOFFMagic 0x99584F46
+.define XLOFFArch  1
 
 .section text
 
@@ -54,16 +62,16 @@ LoadBIOS:
 	mov  long [sp + 4], s0
 
 	la   s0, _data_end ;this is where the BIOS image should be in ROM
-	mov  t0, long [s0 + LOFF_Magic]
-	la   t1, LOFFMagic
+	mov  t0, long [s0 + XLOFF_Magic]
+	la   t1, XLOFFMagic
 	sub  t0, t0, t1
 	beq  t0, .valid1
 
 	b    .notvalid
 
 .valid1:
-	mov  t0, long [s0 + LOFF_Architecture]
-	subi t0, t0, LOFFArch
+	mov  t0, long [s0 + XLOFF_TargetArchitecture]
+	subi t0, t0, XLOFFArch
 	beq  t0, .valid
 
 	b    .notvalid
@@ -73,19 +81,22 @@ LoadBIOS:
 	jal  Puts
 
 	mov  a1, s0
-	mov  a0, long [s0 + LOFF_TextHeader]
+	mov  a0, long [s0 + XLOFF_SectionTableOffset]
 	add  a0, a0, s0
 	jal  CopySection
 
 	mov  a1, s0
-	mov  a0, long [s0 + LOFF_DataHeader]
+	mov  a0, long [s0 + XLOFF_SectionTableOffset]
+	addi a0, a0, SectionHeader_SIZEOF
 	add  a0, a0, s0
 	jal  CopySection
 
-	mov  t0, long [s0 + LOFF_BSSHeader]
+	mov  t0, long [s0 + XLOFF_SectionTableOffset]
+	addi t0, t0, SectionHeader_SIZEOF
+	addi t0, t0, SectionHeader_SIZEOF
 	add  t0, t0, s0
-	mov  t1, long [t0 + Header_SectionSize]
-	mov  t2, long [t0 + Header_LinkedAddress]
+	mov  t1, long [t0 + SectionHeader_DataSize]
+	mov  t2, long [t0 + SectionHeader_VirtualAddress]
 
 	li   a0, 0
 	mov  a1, t1
@@ -93,25 +104,24 @@ LoadBIOS:
 	jal  memset
 
 .zdone:
-	mov  t0, long [s0 + LOFF_EntrySymbol]
+	mov  t0, long [s0 + XLOFF_EntrySymbol]
 	addi t1, t0, 1 ;compare with constant 0xFFFFFFFF
 	beq  t1, .notvalid
 
-	mov  t1, long [s0 + LOFF_SymbolTableOffset]
+	mov  t1, long [s0 + XLOFF_SymbolTableOffset]
 	add  t1, t1, s0
 
 	li   t2, Symbol_sizeof
 	mul  t0, t0, t2
 	add  t0, t0, t1
 
-	mov  t1, long [t0 + Symbol_Section]
-	subi t2, t1, 1
-	bne  t2, .notvalid
+	mov  t1, int [t0 + Symbol_SectionIndexI]
+	bne  t1, .notvalid
 
 	mov  t0, long [t0 + Symbol_Value]
-	mov  t1, long [s0 + LOFF_TextHeader]
+	mov  t1, long [s0 + XLOFF_SectionTableOffset]
 	add  t1, t1, s0
-	mov  t2, long [t1 + Header_LinkedAddress]
+	mov  t2, long [t1 + SectionHeader_VirtualAddress]
 
 	add  s0, t0, t2
 
@@ -141,11 +151,11 @@ LoadBIOS:
 ;a0 - section header
 ;a1 - image offset
 CopySection:
-	mov  t0, long [a0 + Header_SectionOffset]
+	mov  t0, long [a0 + SectionHeader_DataOffset]
 	add  t0, t0, a1 ;t0 now contains the section's address
 
-	mov  t1, long [a0 + Header_SectionSize]
-	mov  t2, long [a0 + Header_LinkedAddress]
+	mov  t1, long [a0 + SectionHeader_DataSize]
+	mov  t2, long [a0 + SectionHeader_VirtualAddress]
 
 	subi sp, sp, 4
 	mov  long [sp], lr
